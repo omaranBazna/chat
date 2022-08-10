4832671bdd2db448cb95e8dab946ab0f24735446
omaran bazna <omran4d@gmail.com>
2022-08-10 19:30:15 -0400

    return the auth state to the main app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { auth } from "firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 function App() {
+  const [user] = useAuthState(auth);
   return <div className="App"></div>;
 }
 
