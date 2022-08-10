ae96eab60cf7e60ec242254df119cd962a8fcdf4
omaran bazna <omran4d@gmail.com>
2022-08-10 19:38:59 -0400

    add google button to the sign in component

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import GoogleButton from "react-google-button";
 const Signin = () => {
-  return <div></div>;
+  return (
+    <div>
+      <GoogleButton />
+    </div>
+  );
 };
 
 export default Signin;
