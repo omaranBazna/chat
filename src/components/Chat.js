36cf38210d3cd3bb86f5732c45a0f903b4351f61
omaran bazna <omran4d@gmail.com>
2022-08-10 20:09:02 -0400

    create message component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,12 @@
 import React from "react";
-
+import { useState, useRef, useEffect } from "react";
+import Message from "./Message";
 const Chat = () => {
+  const scroll = useRef();
   return (
     <>
       <main></main>
+      <span ref={scroll}></span>
     </>
   );
 };
