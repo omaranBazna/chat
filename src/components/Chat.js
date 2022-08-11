96146bb3c59905db324f8c9b8482498dce669a24
omaran bazna <omran4d@gmail.com>
2022-08-10 20:23:52 -0400

    get the messages array from messages collection

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,23 @@
+import { onSnapshot, orderBy, query, collection } from "firebase/firestore";
 import React from "react";
 import { useState, useRef, useEffect } from "react";
 import Message from "./Message";
+
+import { db } from "../firebase";
 const Chat = () => {
   const scroll = useRef();
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    const q = query(collection(db, "messages"), orderBy("timestamp"));
+    const unsubscribe = onSnapshot(q, (querySnapchat) => {
+      let messages = [];
+      querySnapchat.forEach((doc) => {
+        messages.push({ ...doc.data, id: doc.id });
+      });
+      setMessages(messages);
+      return unsubscribe;
+    });
+  }, [messages]);
   return (
     <>
       <main>
