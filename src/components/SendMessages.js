631ef3c1b640f6490952d651a012a97be4d93d49
omaran bazna <omran4d@gmail.com>
2022-08-10 20:56:41 -0400

    send message

--- src/components/SendMessages.js
+++ src/components/SendMessages.js
@@ -1,11 +1,19 @@
+import { addDoc } from "firebase/firestore";
 import React from "react";
 import { useState } from "react";
 import { auth, db } from "../firebase";
+import { serverTimestamp } from "firebase/firestore";
 const SendMessages = () => {
   const [input, setInput] = useState("");
   const sendMessage = async (e) => {
     e.preventDefault();
     const { uid, displayName } = auth.currentUser;
+    await addDoc(collection(db, "messages"), {
+      text: input,
+      name: displayName,
+      uid,
+      timestamp: serverTimestamp(),
+    });
   };
   return (
     <form onSubmit={sendMessage}>
