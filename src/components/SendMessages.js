10150244eeeca689ff5c790653dfb4383e096f66
omaran bazna <omran4d@gmail.com>
2022-08-10 20:51:31 -0400

    handle form send input change

--- src/components/SendMessages.js
+++ src/components/SendMessages.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import { useState } from "react";
 const SendMessages = () => {
+  const [input, setInput] = useState("");
   return (
     <form>
-      <input type="text" placeholder="message" />
+      <input
+        type="text"
+        value={input}
+        placeholder="message"
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
       <button type="submit">Send</button>
     </form>
   );
